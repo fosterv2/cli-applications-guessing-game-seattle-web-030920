75d9c75c384afd0e63eee8d9b88bc3397dc985c2
Valerie Foster <fosterv2@outlook.com>
2020-02-03 23:14:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,9 +3,9 @@ def run_guessing_game
   comp_num = rand(6) + 1
   puts "Choose a number between 1 and 6 or 'exit' to exit"
   user_input = gets.chomp
-  if user_num == "exit"
+  if user_input == "exit"
     puts "Goodbye!"
-  elsif user_num == comp_num
+  elsif user_input == comp_num
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{comp_num}"
